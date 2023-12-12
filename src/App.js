c095cabcffbbf99e3771a94b5d7aecb719e8259c
Yash <yashgarg1703@gmail.com>
2023-12-12 12:29:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,53 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [inputText, setInputText] = useState('');
+  const [summary, setSummary] = useState('');
+  const [wordCount, setWordCount] = useState(0);
+  const [charCount, setCharCount] = useState(0);
+
+  useEffect(() => {
+    // Count characters whenever summary changes
+    setCharCount(summary.length);
+  }, [summary]);
+
+  const summarize = async () => {
+    // Add your summarization logic here or make an API call to the server-side implementation.
+    // Update the 'summary' state with the result.
+
+    // For example, a simple placeholder summarization:
+    const summarizedText = 'This is a summarized version of the input text.';
+    setSummary(summarizedText);
+
+    // Count words in the summarized text
+    const words = summarizedText.split(/\s+/).filter((word) => word.length > 0);
+    setWordCount(words.length);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Text Summarizer</h1>
+        <div className="textarea-container">
+          <textarea
+            value={inputText}
+            onChange={(e) => setInputText(e.target.value)}
+            placeholder="Paste your text here..."
+            className="input-textarea"
+          ></textarea>
+          <button onClick={summarize} className="summarize-btn">
+            Summarize
+          </button>
+          <textarea
+            value={summary}
+            readOnly
+            placeholder="Summary will appear here..."
+            className="summary-textarea"
+          ></textarea>
+          <span className="word-count">Word Count: {wordCount}</span>
+          <span className="char-count">Character Count: {charCount}</span>
+        </div>
       </header>
     </div>
   );
